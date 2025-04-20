9b5453558ab1529b0d1568121c91d2938b2544c5
juacomiranda <jmiranda@live.cl>
2025-04-20 13:57:25 -0400

    card_2

--- src/js/components/Nav.jsx
+++ src/js/components/Nav.jsx
@@ -6,11 +6,11 @@ export const Nav = () => {
         <nav className="navbar navbar-expand-lg nav-color">
             <div className="container-fluid">
                 <a className="navbar-brand text-light ms-5 my-2 fs-3" href="#">Ofertas Wom</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-auto gap-5 mb-2 mb-lg-0">
+                    <ul className="navbar-nav mx-auto gap-5 mb-2 mb-lg-0 ">
                         <li className="nav-item">
                             <a className="nav-link text-light text-nav-dec" aria-current="page" href="#">Planes Empresas</a>
                         </li>
