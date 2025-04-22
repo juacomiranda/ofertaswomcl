4ca8e505dc0df93d1eed0bfa3fcf4c01ef7da439
juacomiranda <jmiranda@live.cl>
2025-04-22 03:44:04 -0400

    catalogo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 			<Nav />
 			<div className="container text-center">
 				<h2 className="mt-5 color_wom1">EQUIPOS PIE  <p className="h2 color_wom2 d-inline">$0</p></h2>
-				<h2 className="color_wom1">CONTRATANDO <p className="h2 color_wom2 d-inline">400 GB</p></h2>
+				<h2 className="color_wom1">CONTRATANDO <p className="h2 color_wom2 d-inline">PLAN 400 GB</p></h2>
 				<hr></hr>
 			</div>
 			<Catalogo />
